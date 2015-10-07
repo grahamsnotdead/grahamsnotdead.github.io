b4961327dba725618cef085a4be02658b90ae4e0
grahamsnotdead <jsgaming442@gmail.com>
2015-10-07 18:01:09 -0500

    Update main.php

--- main.php
+++ main.php
@@ -1,101 +1,48 @@
-<?php
-define('CONFIG_FILE', 'simple-php-proxy_config.php');
-/*
-    Installation note:
-    Copy example.simple-php-proxy_config.php to simple-php-proxy_config.php
-    in the same folder as this script or to one or two levels above it.
-    Optionally you can change the variables right here and not use the config file.
-    For an explanation of the config variables see example.simple-php-proxy_config.php
-*/
-$dest_host = "bash.org";
-$proxy_base_url = '/';
-$proxied_headers = array('Set-Cookie', 'Content-Type', 'Cookie', 'Location');
-// Variables you specify in the config file overwrite variables set above.
-foreach( array('./', '../', '../../') as $path_rel )
-{
-    if( file_exists(dirname(__file__)."/$path_rel" . CONFIG_FILE) )
-    { 
-        include(dirname(__file__)."/$path_rel" . CONFIG_FILE);
-        break;
+
+<?php 
+  $method = $_SERVER['REQUEST_METHOD'];
+  if ($_GET && $_GET['url']) {
+    $headers = getallheaders();
+    $headers_str = [];
+    $url = $_GET['url'];
+    
+    foreach ( $headers as $key => $value){
+      if($key == 'Host')
+        continue;
+      $headers_str[]=$key.":".$value;
     }
-}
-//canonical trailing slash
-$proxy_base_url_canonical = rtrim($proxy_base_url, '/ ') . '/';
-//check if valid
-if( strpos($_SERVER['REQUEST_URI'], $proxy_base_url) !== 0 )
-{
-    die("The config paramter \$prox_base_url \"$proxy_base_url\" that you specified 
-        does not match the beginning of the request URI: ".
-        $_SERVER['REQUEST_URI']);
-}
-//remove base_url and optional index.php from request_uri
-$proxy_request_url = substr($_SERVER['REQUEST_URI'], strlen($proxy_base_url_canonical));
-if( strpos($proxy_request_url, 'index.php') === 0 )
-{
-    $proxy_request_url = ltrim(substr($proxy_request_url, strlen('index.php')), '/');
-}
-//final proxied request url
-$proxy_request_url = "http://" . rtrim($dest_host, '/ ') . '/' . $proxy_request_url;
-/* Init CURL */
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $proxy_request_url);
-curl_setopt($ch, CURLOPT_AUTOREFERER, 1);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_HEADER, 1);
-curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
-curl_setopt($ch, CURLOPT_HTTPHEADER, array('Expect:'));
-curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_0);
-/* Collect and pass client request headers */
-if(isset($_SERVER['HTTP_COOKIE']))     
-{ 
-    $hdrs[]="Cookie: " . $_SERVER['HTTP_COOKIE'];        
-}
-if(isset($_SERVER['HTTP_USER_AGENT'])) 
-{ 
-    $hdrs[]="User-Agent: " . $_SERVER['HTTP_USER_AGENT']; 
-}
-curl_setopt($ch, CURLOPT_HTTPHEADER, $hdrs);
-/* pass POST params */
-if( sizeof($_POST) > 0 )
-{ 
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $_POST); 
-}
-$res = curl_exec($ch);
-curl_close($ch);
-/* parse response */
-list($headers, $body) = explode("\r\n\r\n", $res, 2);
-$headers = explode("\r\n", $headers);
-$hs = array();
-foreach($headers as $header)
-{
-    if( false !== strpos($header, ':') )
-    {
-        list($h, $v) = explode(':', $header);
-        $hs[$h][] = $v;
+    $ch = curl_init($url);
+    curl_setopt($ch,CURLOPT_URL, $url);
+    if( $method !== 'GET') {
+      curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
     }
-    else 
-    {
-        $header1  = $header;
+    if($method == "PUT" || $method == "PATCH" || ($method == "POST" && empty($_FILES))) {
+      $data_str = file_get_contents('php://input');
+      curl_setopt($ch, CURLOPT_POSTFIELDS, $data_str);
+      //error_log($method.': '.$data_str.serialize($_POST).'\n',3, 'err.log');
     }
-}
-/* set headers */
-list($proto, $code, $text) = explode(' ', $header1);
-header($_SERVER['SERVER_PROTOCOL'] . ' ' . $code . ' ' . $text);
-foreach($proxied_headers as $hname)
-{
-    if( isset($hs[$hname]) )
-    {
-        foreach( $hs[$hname] as $v )
-        {
-            if( $hname === 'Set-Cookie' ) 
-            {
-                header($hname.": " . $v, false);
-            }
-            else
-            {
-                header($hname.": " . $v);
-            }
+    elseif($method == "POST") {
+      $data_str = array();
+      if(!empty($_FILES)) {
+        foreach ($_FILES as $key => $value) {
+          $full_path = realpath( $_FILES[$key]['tmp_name']);
+          $data_str[$key] = '@'.$full_path;
         }
+      }
+      //error_log($method.': '.serialize($data_str+$_POST).'\n',3, 'err.log');
+      curl_setopt($ch, CURLOPT_POSTFIELDS, $data_str+$_POST);
     }
-}
-die($body);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt( $ch, CURLOPT_HTTPHEADER, $headers_str );
+    $result = curl_exec($ch);
+    curl_close($ch);
+    header('Content-Type: application/json');
+    echo $result;
+  }
+  else {
+    echo $method;
+    var_dump($_POST);
+    var_dump($_GET);
+    $data_str = file_get_contents('php://input');
+    echo $data_str;
+  }
